73ba9e0d35a3c26a1a79422124c675886f875f6f
RodrigoDelgado005 <162932332+RodrigoDelgado005@users.noreply.github.com>
2024-04-11 19:10:30 -0300

    first commit

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+function crearLista() {
+    const dias = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo']
+    let cadena = ""
+    for (let dia of dias)
+        cadena += `<li>${dia}</li>`
+    document.getElementById("lista1").innerHTML = cadena
+}
